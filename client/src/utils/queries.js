0abae2d67c96a745c53136c908f2e9ad2bbc1ddf
kasthana112 <kanika.asthana@gmail.com>
2023-04-11 12:03:21 -0500

    created queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+export const Get_me = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
