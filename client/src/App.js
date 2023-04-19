ffcc1509781360a927a3708c88dcc5a71c1e7e23
kasthana112 <kanika.asthana@gmail.com>
2023-04-19 02:16:07 -0500

    fixed app.js errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,33 @@
 import React from 'react';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-
-const httpLink = createHttpLink({
-  uri: "/graphql",
+import { ApolloProvider } from '@apollo/client';
+import {ApolloClient} from '@apollo/client';
+import {InMemoryCache} from '@apollo/client';
+import {createHttpLink} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+ const httpLink = createHttpLink({
+  uri: '/graphql',
 });
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
-  // return the headers to the context so httpLink can read them
+  const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
+
 function App() {
-  return (
+  return (  
     <ApolloProvider client={client}>
     <Router>
       <>
@@ -42,11 +35,11 @@ function App() {
         <Routes>
           <Route 
             path='/' 
-            element={<SearchBooks />} 
+            element={<SearchBooks/>} 
           />
           <Route 
             path='/saved' 
-            element={<SavedBooks />} 
+            element={<SavedBooks/>} 
           />
           <Route 
             path='*'
